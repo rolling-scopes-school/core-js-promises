a67449d4aacb28cd4604355a1ec522a543a05bad
Mikhail Aleinik <6424156@tut.by>
2024-02-20 23:38:56 +0300

    feat: added default files

---
+++ test/core-js-promises-tests.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const tasks = require('../src/core-js-promises-tasks');
+const forbidden = require('../extensions/forbidden');
+it.optional = require('../extensions/it-optional');
+
+describe('core-js-promises', () => {
+  it.optional('', () => {});
+});
